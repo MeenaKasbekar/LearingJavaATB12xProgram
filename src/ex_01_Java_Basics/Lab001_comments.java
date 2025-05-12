343421d825b540ad9ec991da4a3655ae1c37cf74
Meena Kasbekar <mkasbekar23@gmail.com>
2025-05-13 00:13:26 +0530

    comments lab

---
+++ src/ex_01_Java_Basics/Lab001_comments.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+public class Lab001_comments {
+    public static void main(String[] args) {
+        System.out.println("Comments in Java");
+
+        //This is Single line comment
+
+        /*
+        * This is multiline comment
+        * not printed on terminal
+        * used to give more info of the code
+         */
+    }
+}
