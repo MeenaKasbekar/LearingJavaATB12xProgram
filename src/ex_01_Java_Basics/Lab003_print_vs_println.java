35be32449863e30a3a140ad074ee14cfa857cb62
Meena Kasbekar <mkasbekar23@gmail.com>
2025-05-13 00:26:05 +0530

    Print VS Println

---
+++ src/ex_01_Java_Basics/Lab003_print_vs_println.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab003_print_vs_println {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi");
+        System.out.println("Meena"); //println means next line
+        System.out.println("#################");
+        System.out.print("Hi,");
+        System.out.print("Meena"); //only print means in the same line
+    }
+}
