2a95063992f1023b64d3e279576c1b6bc0ec098c
Meena Kasbekar <mkasbekar23@gmail.com>
2025-05-13 23:52:46 +0530

    Task for printf formatting

---
+++ src/ex_02_Java_Basics_Part2/Task1.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basics_Part2;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int a=5;
+
+        System.out.printf("5 X 1= %d\n",5*1);
+        System.out.printf("5 X 2= %d\n",5*2);
+        System.out.printf("5 X 3= %d\n",5*3);
+        System.out.printf("5 X 4= %d\n",5*4);
+        System.out.printf("5 X 5= %d\n",5*5);
+        System.out.printf("5 X 6= %d\n",5*6);
+        System.out.printf("5 X 7= %d\n",5*7);
+        System.out.printf("5 X 8= %d\n",5*8);
+        System.out.printf("5 X 9= %d\n",5*9);
+        System.out.printf("5 X 10= %d\n",5*10);
+    }
+}
