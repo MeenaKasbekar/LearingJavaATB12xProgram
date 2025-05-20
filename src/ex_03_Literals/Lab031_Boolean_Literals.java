2cf18908217cc136ca26a05e85292a0f504cc8d3
Meena Kasbekar <mkasbekar23@gmail.com>
2025-05-20 19:50:16 +0530

    Literals Codes

---
+++ src/ex_03_Literals/Lab031_Boolean_Literals.java
@@ -0,0 +1,11 @@
+package ex_03_Literals;
+
+public class Lab031_Boolean_Literals {
+    public static void main(String[] args) {
+
+        boolean meena_is_married= true;
+        boolean meena_is_unmarried=false;
+        //boolean any_value=123;
+        //boolean will only except true or false values not any other
+    }
+}
---
+++ src/ex_03_Literals/Lab032_Literals_P3.java
@@ -0,0 +1,10 @@
+package ex_03_Literals;
+
+public class Lab032_Literals_P3 {
+    public static void main(String[] args) {
+
+        int Enum=9;
+        //int enum=10;....enum is a keyword in java
+        System.out.println(Enum);
+    }
+}
