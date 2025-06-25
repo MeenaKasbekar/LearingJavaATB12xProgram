e219c6985d37d7cce5b95f852376f2b359ae78c7
Meena Kasbekar <mkasbekar23@gmail.com>
2025-06-25 23:43:54 +0530

    If-else Task

---
+++ src/Tasks/Basic_If_Else/Grade_System.java
@@ -0,0 +1,36 @@
+package Tasks.Basic_If_Else;
+
+import java.util.Scanner;
+
+public class Grade_System {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter your marks: ");
+        int marks= scanner.nextInt();
+
+        if(marks >0 && marks<=100){
+            if(marks>90 && marks<100){
+                System.out.println("A+");
+            } else if (marks>80 && marks<89) {
+                System.out.println("A");
+
+            } else if (marks>70 && marks<79) {
+                System.out.println("B");
+
+            } else if (marks>60 && marks<69) {
+                System.out.println("C");
+
+            } else if (marks>50 && marks<59) {
+                System.out.println("D");
+
+            } else if (marks>40 && marks<49) {
+                System.out.println("E");
+            }else {
+                System.out.println("fail");
+            }
+        }else {
+            System.out.println("Please enter valid marks");
+        }
+    }
+}
---
+++ src/Tasks/Basic_If_Else/Max_3_no.java
@@ -0,0 +1,29 @@
+package Tasks.Basic_If_Else;
+
+import java.util.Scanner;
+
+public class Max_3_no {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Enter First Number");
+        int num1=scanner.nextInt();
+
+        System.out.print("Enter Second Number");
+        int num2=scanner.nextInt();
+
+        System.out.print("Enter Third Number");
+        int num3=scanner.nextInt();
+
+        if(num1>num2 && num1>num3){
+            System.out.println("Maximum Number " +num1);
+        } else if (num2>num1 && num2>num3) {
+            System.out.println("Maximum Number " +num2);
+        }
+        else {
+            System.out.println("Maximum Number " +num3);
+        }
+
+    }
+}
