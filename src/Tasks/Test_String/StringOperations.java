066d3332f24a7fdb3090accd86614fd07cf15523
Meena Kasbekar <mkasbekar23@gmail.com>
2025-06-28 11:50:03 +0530

    Task test

--- src/Tasks/Test_String/StringOperations.java
+++ src/Tasks/Test_String/StringOperations.java
@@ -3,28 +3,19 @@ package Tasks.Test_String;
 public class StringOperations {
     public static void main(String[] args) {
         // 1. String Declaration
-        String firstName = "Meena";
-        String lastName = "Kasbekar";
+        String str1 = "Hello";
+        String str2 = "hello";
+        String str3 ="Hello";
 
-        // 2. Concatenation
-        String fullName = firstName + " " + lastName;
-        System.out.println("Full Name: " + fullName);
+        //equals
+        System.out.println(str1.equals(str2));
 
-        // 3. Length of the string
-        int length = fullName.length();
-        System.out.println("Length: " + length);
+        //equalsignorecase
+        System.out.println(str1.equalsIgnoreCase(str2));
 
-        // 4. Substring
-        String sub = fullName.substring(0, 8);
-        System.out.println("Substring " + sub);
+        //compare to
+        System.out.println(str1.compareTo(str2));
 
-        // 5. Character extraction
-        char initial = fullName.charAt(0);
-        System.out.println("First character: " + initial);
-
-        // 6. Last character
-        char lastChar = fullName.charAt(fullName.length() - 1);
-        System.out.println("Last character: " + lastChar);
     }
 }
 
