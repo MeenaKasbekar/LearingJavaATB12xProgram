126e5fc2fc7e0fdc675e4f8ef0b6c26d77cbf256
Meena Kasbekar <mkasbekar23@gmail.com>
2025-06-28 11:56:37 +0530

    Task test

---
+++ src/Tasks/Test_String/BasicOperations.java
@@ -0,0 +1,12 @@
+package Tasks.Test_String;
+
+public class BasicOperations {
+    public static void main(String[] args) {
+
+        String str="Hello World";
+
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.substring(6));
+    }
+}
